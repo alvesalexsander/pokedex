0db27f7246ae9005b96878a16909d3253963e4f5
sashaclimax <alvesalexsander@live.com>
2019-01-28 22:26:58 -0200

    Implementações JS
    -Implementado o sistema de busca com os botões numéricos.
    
    -Implementado o tratamento dos nomes dos pokemon antes de mostrar

--- js/input.js
+++ js/input.js
@@ -1,3 +1,14 @@
-//var searchData = $("#search").serialize();
-//console.log(searchData);
+formField = document.getElementById("form__field");
 
+var key = document.getElementsByClassName("key");
+
+for(var i=0;i<key.length;i++){
+    key[i].onclick = updateField;
+}
+
+// console.log(key);
+
+function updateField(){
+    formField.value = formField.value + this.innerHTML;
+    // console.log("clicked");
+}
